1f1c9cf005f45e1fd70b04c73a6b090e5adc71ed
katsuya-urata <katsuya.urata0403.shishi@gmail.com>
2022-07-23 00:06:39 +0900

    first commit

--- Main_app.py
+++ Main_app.py
@@ -19,7 +19,9 @@ st.caption('これはPythonプログラムで作成されたWebアプリです')
 #xlsxデータの処理用
 #◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆
 name_xlsx = st.text('洋日配サマリー表集約処理_xlsx')
-uploaded_files_xlsx = st.file_uploader('ファイルアップロード', type='xlsx')
+#uploaded_files_xlsx = st.file_uploader('ファイルアップロード', type='xlsx')
+uploaded_files_xlsx = st.file_uploader('ファイルアップロード')
+
 submit_btn_xlsx = st.button('xlsx処理実行')
 #ボタンが押されたら処理を実行する
 if submit_btn_xlsx:
