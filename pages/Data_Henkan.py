aa6ce3db1a6e55866bf6c135900bd0e05d7aa26f
katsuya-urata <katsuya.urata0403.shishi@gmail.com>
2022-07-23 01:45:32 +0900

    first commit

--- pages/Data_Henkan.py
+++ pages/Data_Henkan.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[24]:
+# In[28]:
 
 
 import streamlit as  st
@@ -11,7 +11,7 @@ import xlsxwriter
 import openpyxl
 
 
-# In[25]:
+# In[29]:
 
 
 #◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆
@@ -24,7 +24,8 @@ submit_btn_xlsx = st.button('xlsx処理実行')
 #ボタンが押されたら処理を実行する
 if submit_btn_xlsx:
     _df_xlsx = pd.read_excel(uploaded_files_xlsx)
-    #st.dataframe(_df_xlsx)
+    _df_xlsx = _df_xlsx['JAN']
+    _df_xlsx
     
     #エクセルでの書き出しはかなり特殊なようでこのような対応が必要
     xlsx_dl = io.BytesIO()
@@ -35,7 +36,7 @@ if submit_btn_xlsx:
         st.download_button(label='エクセルダウンロード', data=xlsx_dl, file_name='洋日配サマリ集計後.xlsx', mime='application/vnd.ms-excel')
 
 
-# In[26]:
+# In[30]:
 
 
 #◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆
@@ -52,4 +53,10 @@ if submit_btn_csv:
     #出力するデータが表示されたら、ダウンロードボタンが出てくる
     csv_dl = _df_csv.to_csv()
     st.download_button(label='ＣＳＶダウンロード', data=csv_dl, file_name='洋日配サマリ集計後.csv')
+
+
+# In[ ]:
+
+
+
 
