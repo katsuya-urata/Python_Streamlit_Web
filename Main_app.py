c9e82a525cf95557c08b28430a7c65ee7624e1c1
katsuya-urata <katsuya.urata0403.shishi@gmail.com>
2022-07-23 00:04:21 +0900

    first commit

--- Main_app.py
+++ Main_app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[2]:
+# In[4]:
 
 
 import streamlit as  st #Webアプリ作成の為
@@ -10,6 +10,21 @@ import io
 
 st.title('Pythonアプリケーション')
 st.caption('これはPythonプログラムで作成されたWebアプリです')
+
+
+# In[5]:
+
+
+#◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆
+#xlsxデータの処理用
+#◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆
+name_xlsx = st.text('洋日配サマリー表集約処理_xlsx')
+uploaded_files_xlsx = st.file_uploader('ファイルアップロード', type='xlsx')
+submit_btn_xlsx = st.button('xlsx処理実行')
+#ボタンが押されたら処理を実行する
+if submit_btn_xlsx:
+    _df_xlsx = pd.read_excel(uploaded_files_xlsx)
+    _df_xlsx
 
 
 # In[ ]:
