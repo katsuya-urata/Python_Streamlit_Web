345bd1b018318bd2bdfa03d56eab88447faef98c
katsuya-urata <katsuya.urata0403.shishi@gmail.com>
2022-07-23 00:00:10 +0900

    first commit

---
+++ Main_app.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+import streamlit as  st #Webアプリ作成の為
+import pandas as pd
+import io
+
+st.title('Pythonアプリケーション')
+st.caption('これはPythonプログラムで作成されたWebアプリです')
+
+
+# In[ ]:
+
+
+
+
